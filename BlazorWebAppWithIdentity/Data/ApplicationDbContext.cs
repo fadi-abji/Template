de848e74b74b4e1b088fe4340f4f01f33831de2c
Fadi Abji <fadi@wexer.se>
2024-11-13 14:21:24 +0100

    updates

---
+++ BlazorWebAppWithIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorWebAppWithIdentity.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<BlazorWebAppWithIdentity.Models.Movie> Movie { get; set; } = default!;
+    }
+}
